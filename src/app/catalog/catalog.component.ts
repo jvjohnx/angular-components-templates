9219d84ed419989738b8d405d46ac683fbaca30e
johnson <johnson@jvjohn.org>
2025-08-17 22:13:32 -0400

    Creating a new pipe called fileterByCategory.  This pipe has an issue where, when you first filter by Arms and then Add New, which adds an Arm, the Arm is not visbile in the filtered list.  Because the change detection will not work since we are mutating the products array in catalog.component.ts addProduct() method.

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 import { ProductDetailsComponent } from "../product-details/product-details.component";
 import { IProduct } from '../product.model';
 import allProducts from '../products.json';
+import {FilterByCategoryPipe} from '../filter-by-category-pipe';
 
 @Component({
   selector: 'bot-catalog',
-  imports: [ProductDetailsComponent],
+  imports: [ProductDetailsComponent, FilterByCategoryPipe],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
 export class CatalogComponent {
   products: IProduct[] = allProducts;
+  categoryFilter: string | null = null;
 
+  addProduct() {
+    //here mutation is used to push a new Product to the array
+    //if this is used for creating a "pure" pipe with products, mutation of this array will not cause change detection
+    // and the new product will not be rendered
+    this.products.push({
+      "id": 6,
+      "description": "Something new",
+      "name": "New Arm",
+      "imageName": "arm-propeller.png",
+      "category": "arms",
+      "price": 100,
+      "discount": 0,
+    });
+  }
 }
---
+++ src/app/filter-by-category-pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {IProduct} from './product.model';
+
+@Pipe({
+  name: 'filterByCategory',
+  // pure: false //Don't do this.  This will fix the change detection issue .. but will have performance problems.
+})
+export class FilterByCategoryPipe implements PipeTransform {
+
+  transform(products: IProduct[], category: string | null): IProduct[] {
+    if( !category ){
+      return products
+    }
+    return products.filter(p => p.category===category);
+  }
+}
